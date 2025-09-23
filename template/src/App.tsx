b78b53d0050b4e50eceb753b0595a1e3fd917c28
rossana87 <rossana.ventrella@gmail.com>
2025-09-23 10:08:32 +0200

    chore - phone input

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -1,6 +1,18 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [value, setValue] = useState('')
+
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(onFormat(event.target.value));
+  }
+
+  const onSubmit = () => {
+    setValue('')
+  }
+
   return (
     <div>
       <input
@@ -8,11 +20,35 @@ function App() {
         name="tel"
         placeholder="(555) 555-5555"
         id="tel"
-        required
+        value={value}
+        onChange={onChange}
       />
-      <button>Submit</button>
+      <button
+        disabled={value.length < 14}
+        onClick={onSubmit}
+      >Submit</button>
     </div>
   )
 }
 
 export default App
+
+const onFormat = (str: string): string => {
+  const rawString = str.replace(/\D/g, '');
+  let output = ''
+
+  if (rawString.length > 0) {
+    output += '('
+    output += rawString.substring(0, 3)
+  }
+  if (rawString.length > 3) {
+    output += ') '
+    output += rawString.substring(3, 6)
+  }
+  if (rawString.length > 6) {
+    output += '-'
+    output += rawString.substring(6, 10)
+  }
+
+  return output
+}
